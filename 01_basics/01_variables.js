d86fa77ebac2d511537108803b2113d91e63a5ea
Nitish <nitishkumar03382@gmail.com>
2024-05-13 17:42:42 +0000

    Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 13423
+var accountName = "Nitish"
+let accountEmail = "nk@google.com"
+accountCIty = "Patna"
+
+// accountID = 23232  //TypeError: Assignment to constant variable
+console.table([accountID, accountName, accountEmail, accountCIty]);
+/*
+We don`t use var in modern JavaScript because var is inconsistent with scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version
+// alert("Hello") // We are using Node JS not browser
+//DATA TYPES
+/*
+1. String => "Name"
+2. Number => 23
+3. Boolean => true/false
+4. undefined
+5. symbol => unique
+6. Object (null is a object)
+*/
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "Hello");
+console.log(typeof 2+4);
+console.log(typeof true);
