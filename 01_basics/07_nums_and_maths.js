24695928bd71b3638b6dc200aa24704047dfdb9c
Nitish <nitishkumar03382@gmail.com>
2024-05-31 14:58:30 +0000

    Math and Num and DOM

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,22 @@
+// NUMBERS
+const score = 400
+// console.log(score)
+const balance = new Number(35000)
+// console.log(balance)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+const decimal_num = 23.8299
+// console.log(decimal_num.toPrecision(3));
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// >>>>>>>>>>>>>>>MATHS<<<<<<<<<<<<<<<<<<<<
+// console.log('Abs:', Math.abs(-1))
+// console.log('Round:', Math.round(12.5));
+// ceil, floor, min, max
+// ## RANDOM
+// console.log('Random:', Math.floor(Math.random() * 10) + 1)
+const minm = 10
+const maxm = 21
+console.log(Math.floor(Math.random() * (maxm - minm + 1)) + minm)
+
+
